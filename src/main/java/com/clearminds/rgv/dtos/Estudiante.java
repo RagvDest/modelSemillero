b0942e64ae2aaa4818fc9c45a5b38b41ef97f1a8
RagvDest <rennygorozabel@hotmail.com>
2020-12-10 09:19:38 -0500

    Modificar Estudiantes DTO > Edad

--- src/main/java/com/clearminds/rgv/dtos/Estudiante.java
+++ src/main/java/com/clearminds/rgv/dtos/Estudiante.java
@@ -7,6 +7,7 @@ package com.clearminds.rgv.dtos;
 public class Estudiante {
 	private String nombre;
 	private String apellido;
+	private int edad;
 	private int id;
 
 	public Estudiante() {
@@ -17,16 +18,25 @@ public class Estudiante {
 		this.apellido = apellido;
 	}
 
-	public Estudiante(String nombre, String apellido, int id) {
+	public Estudiante(String nombre, String apellido, int edad, int id) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.edad = edad;
 		this.id = id;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
 	}
 
 	@Override
 	public String toString() {
-		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", id=" + id + "]";
 	}
 
 	public String getNombre() {
